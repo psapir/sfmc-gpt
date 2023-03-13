534c3fcf3d465444fc4ff6c290e1c8dbea2f03c7
Pato Sapir <patriciosapir@gmail.com>
2023-03-12 20:48:25 -0400

    Update ChatGPTGenerator.js

--- ChatGPTGenerator.js
+++ ChatGPTGenerator.js
@@ -2,11 +2,13 @@
     Platform.Load("Core","1");
     var variations = Variable.GetValue("@variations");
     var prompt = Variable.GetValue("@prompt");
+    var maxTokens = Variable.GetValue("@maxTokens");
     var emailName = Attribute.GetValue("emailname_");
     
    try{
     
     if(!variations) variations = 5;
+    if(!maxTokens) maxTokens = 30;
   
     var generatedContentDE = DataExtension.Init("ChatGPTGeneratedContent");
     var generatedContentRows = generatedContentDE.Rows.Lookup(["EmailName"], [emailName]);
@@ -15,17 +17,18 @@
        generatedContentRows = [];
         var url = 'https://api.openai.com/v1/completions';
         var contentType = 'application/json';
-        var payload = '{ "model": "text-davinci-003", "prompt": "Write '+ variations +' alternatives for this text separated by | without including the original one: '+prompt+'", "temperature": 0.7, "max_tokens": 256 }';
+        var payload = '{ "model": "text-davinci-003", "prompt": "Write an alternative for this text: '+prompt+'", "temperature": 0.5, "max_tokens": '+maxTokens+',"n":'+variations+'}';
+      
         var headerNames = ["Authorization"];
-        var headerValues = ["Bearer YOUR_CHATGPT_API_HERE"];
+        var headerValues = ["Bearer YOUR_CHATGPT_API_GOES_HERE"];
         var result = HTTP.Post(url, "application/json", payload, headerNames, headerValues);
     
         var jsonResult = Platform.Function.ParseJSON(result.Response[0]);
         
-        var resultsArray = jsonResult.choices[0].text.split("|");
+        var resultsArray = jsonResult.choices;
         
         for (i = 0; i < resultsArray.length; i++) {
-           generatedContentRows.push({EmailName:emailName,Variation:i+1,GeneratedText:resultsArray[i]});
+           generatedContentRows.push({EmailName:emailName,Variation:i+1,GeneratedText:resultsArray[i].text});
 
         }
         
